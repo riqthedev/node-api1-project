2c363db58bded10418544498926cc2c1269ed002
riqthedev <riqthedev@gmail.com>
2021-09-28 08:39:02 -0400

    able to grab all users and users by id

--- api/server.js
+++ api/server.js
@@ -1,3 +1,44 @@
-// BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const { rmdirSync } = require('fs')
+const User = require('./users/model')
+const server = express()
+
+server.get('/api/users', (req,res) => {
+    User.find()
+    .then(users => {
+     
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "error getting users",
+            err: err.message,
+            stack: err.stack
+        })
+    })
+    
+})
+
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "User not found",
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+
+server.use('*', (req,res) => {
+    res.status(404).json({
+        message: "not found"
+    })
+})
+
+module.exports = server; 
